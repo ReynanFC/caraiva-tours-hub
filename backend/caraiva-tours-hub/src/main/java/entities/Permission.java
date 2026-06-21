4ff0d0ae2997405fab24c097ada0544ad4ab94c6
Reynan <reynandan11@gmail.com>
2026-06-21 19:47:00 -0300

    feat: add entitie Permission

---
+++ backend/caraiva-tours-hub/src/main/java/entities/Permission.java
@@ -0,0 +1,56 @@
+package entities;
+
+import entities.enums.UserRole;
+import jakarta.persistence.*;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+@Table(name= "permission")
+public class Permission implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name= "permission_id")
+    private Long id;
+    private UserRole role;
+
+    public Permission() {}
+
+    public Permission(UserRole role) {
+        this.role = role;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public UserRole getRole() {
+        return role;
+    }
+
+    public void setRole(UserRole role) {
+        this.role = role;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Permission that = (Permission) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
---
+++ backend/caraiva-tours-hub/src/main/java/entities/enums/UserRole.kt
@@ -0,0 +1,6 @@
+package entities.enums
+
+enum class UserRole {
+    ADMIN,
+    EMPLOYEE
+}
