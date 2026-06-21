ad9016fc793fcb7ace8bc1892653620d31fccce9
Reynan <reynandan11@gmail.com>
2026-06-21 00:26:59 -0300

    feat: initial project structure for backend and frontend

---
+++ backend/caraiva-tours-hub/src/main/java/com/caraivatours/hub/CaraivaToursHubApplication.java
@@ -0,0 +1,13 @@
+package com.caraivatours.hub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaraivaToursHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaraivaToursHubApplication.class, args);
+	}
+
+}
---
+++ backend/caraiva-tours-hub/src/test/java/com/caraivatours/hub/CaraivaToursHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caraivatours.hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaraivaToursHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
