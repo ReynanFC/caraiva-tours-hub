baeff9531f56c28b5c2132dc38db8aed4c93bf1f
Reynan <reynandan11@gmail.com>
2026-06-27 15:16:04 -0300

    feat: add RefreshTokenStore and adjust in JwtTokenProvider

--- backend/caraiva-tours-hub/src/main/java/com/caraivatours/hub/user/UserMapper.java
+++ backend/caraiva-tours-hub/src/main/java/com/caraivatours/hub/user/UserMapper.java
@@ -1,11 +1,14 @@
 package com.caraivatours.hub.user;
 
+import com.caraivatours.hub.auth.entity.Permission;
 import com.caraivatours.hub.user.dto.request.UserRegistrationDTO;
 import com.caraivatours.hub.user.dto.response.UserSummaryDTO;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingConstants;
 import org.mapstruct.ReportingPolicy;
+
+import java.util.List;
 
 @Mapper(
         componentModel = MappingConstants.ComponentModel.SPRING,
@@ -19,9 +22,17 @@ public interface UserMapper {
     @Mapping(target = "pixKey", ignore = true)
     @Mapping(target = "permission", ignore = true)
     @Mapping(target = "bookings", ignore = true)
+    @Mapping(target = "enabled", ignore = true)
     @Mapping(target = "historyChange", ignore = true)
     @Mapping(target = "createdAt", ignore = true)
     User toEntity(UserRegistrationDTO dto);
 
+    @Mapping(target = "userName", source = "username")
+    @Mapping(target = "role", source = "permission")
     UserSummaryDTO toDTO(User user);
+
+    default String mapPermissionsToRole(List<Permission> permissions) {
+        if (permissions == null || permissions.isEmpty()) return null;
+        return permissions.get(0).getRole().name();
+    }
 }
